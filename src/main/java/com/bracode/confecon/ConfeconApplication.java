731db6aabb61b82ee1b03a438f1c81e735cb9fed
Leo Hawk <leonardofalcaog@gmail.com>
2018-12-08 14:56:52 -0200

    first commit

---
+++ src/main/java/com/bracode/confecon/ConfeconApplication.java
@@ -0,0 +1,12 @@
+package com.bracode.confecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfeconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfeconApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bracode/confecon/ConfeconApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bracode.confecon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfeconApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
